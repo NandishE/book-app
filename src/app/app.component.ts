53f15010bbba035c391f59ee98bad8683f8e0226
nandish.e <nandishwar11@gmail.com>
2020-06-12 16:03:48 +0530

    book-application

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Book } from './book';
+import { BOOKS} from './mock-books';
+@Component({
+  selector: 'books-list',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  booksList: Book[] = BOOKS;
+  selectedBook: Book;
+
+  getBookDetails(isbn: number) {
+    this.selectedBook = this.booksList.find(book => book.isbn === isbn);
+  }
+}
+
---
+++ src/app/book.ts
@@ -0,0 +1,9 @@
+export class Book {
+    isbn: number;
+    title: string;
+    authors: string;
+    published: string;
+    description: string;
+    coverImage: string;
+  }
+  
